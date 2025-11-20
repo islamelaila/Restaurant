e23878c0a97baecd3cebc4376cc162baa350422a
islam <eslamahmed99917@gmail.com>
2025-11-20 15:47:12 +0200

    modified JWT-secured authentication

--- src/main/java/com/spring/boot/config/jwt/TokenHandler.java
+++ src/main/java/com/spring/boot/config/jwt/TokenHandler.java
@@ -1,85 +1,85 @@
-//package com.spring.boot.config.jwt;
-//import com.spring.boot.dto.UsersDto;
-//import com.spring.boot.helper.JwtToken;
-//import com.spring.boot.service.UsersService;
-//import io.jsonwebtoken.Claims;
-//import io.jsonwebtoken.JwtBuilder;
-//import io.jsonwebtoken.JwtParser;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.security.Keys;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Lazy;
-//import org.springframework.stereotype.Component;
-//import java.nio.charset.StandardCharsets;
-//import java.security.Key;
-//import java.time.Duration;
-//import java.util.Date;
-//import java.util.Objects;
-//
-//@Component
-//public class TokenHandler {
-//
-//
-//    private String secret ;
-//
-//    private Duration time ;
-//
-//    private JwtBuilder jwtBuilder ;
-//
-//    private JwtParser jwtParser ;
-//
-//    @Lazy
-//    @Autowired
-//    private UsersService usersService ;
-//
-//
-//    public TokenHandler(JwtToken jwtToken ) {
-//        this.secret = jwtToken.getSecret();
-//        this.time = jwtToken.getTime();
-//        Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-//        jwtBuilder = Jwts.builder().signWith(key);
-//        jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
-//
-//    }
-//
-//    public String CreateToken(UsersDto usersDto) {
-//        Date issuDate = new Date() ;
-//        Date expireDate = Date.from(issuDate.toInstant().plus(time));
-//        jwtBuilder.setSubject(usersDto.getUsername());
-//        jwtBuilder.setIssuedAt(issuDate);
-//        jwtBuilder.setExpiration(expireDate);
-//        return jwtBuilder.compact();
-//    }
-//
-//
-//
-//
-//    public UsersDto VaildateToken(String token) {
-//        try {
-//
-//            if (!jwtParser.isSigned(token)){
-//                return null ;
-//            }
-//
-//            Claims claims = jwtParser.parseClaimsJws(token).getBody();
-//            Date issueDate = claims.getIssuedAt();
-//            Date expireDate = claims.getExpiration();
-//            String username = claims.getSubject();
-//
-//            UsersDto usersDto = usersService.getUserByUsername(username);
-//
-//            boolean isTokenValid =expireDate.after(issueDate) && expireDate.after(new Date());
-//
-//            if (isTokenValid && Objects.nonNull(usersDto)){
-//                return usersDto ;
-//            }
-//
-//            return null ;
-//
-//        }catch (Exception e){
-//            return null ;
-//        }
-//
-//    }
-//
-//}
+package com.spring.boot.config.jwt;
+import com.spring.boot.dto.UsersDto;
+import com.spring.boot.helper.JwtToken;
+import com.spring.boot.service.UsersService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Duration;
+import java.util.Date;
+import java.util.Objects;
+
+@Component
+public class TokenHandler {
+
+
+    private String secret ;
+
+    private Duration time ;
+
+    private JwtBuilder jwtBuilder ;
+
+    private JwtParser jwtParser ;
+
+    @Lazy
+    @Autowired
+    private UsersService usersService ;
+
+
+    public TokenHandler(JwtToken jwtToken ) {
+        this.secret = jwtToken.getSecret();
+        this.time = jwtToken.getTime();
+        Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        jwtBuilder = Jwts.builder().signWith(key);
+        jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
+
+    }
+
+    public String CreateToken(UsersDto usersDto) {
+        Date issuDate = new Date() ;
+        Date expireDate = Date.from(issuDate.toInstant().plus(time));
+        jwtBuilder.setSubject(usersDto.getUsername());
+        jwtBuilder.setIssuedAt(issuDate);
+        jwtBuilder.setExpiration(expireDate);
+        return jwtBuilder.compact();
+    }
+
+
+
+
+    public UsersDto VaildateToken(String token) {
+        try {
+
+            if (!jwtParser.isSigned(token)){
+                return null ;
+            }
+
+            Claims claims = jwtParser.parseClaimsJws(token).getBody();
+            Date issueDate = claims.getIssuedAt();
+            Date expireDate = claims.getExpiration();
+            String username = claims.getSubject();
+
+            UsersDto usersDto = usersService.getUserByUsername(username);
+
+            boolean isTokenValid =expireDate.after(issueDate) && expireDate.after(new Date());
+
+            if (isTokenValid && Objects.nonNull(usersDto)){
+                return usersDto ;
+            }
+
+            return null ;
+
+        }catch (Exception e){
+            return null ;
+        }
+
+    }
+
+}
--- src/main/java/com/spring/boot/filter/AuthFilter.java
+++ src/main/java/com/spring/boot/filter/AuthFilter.java
@@ -1,52 +1,52 @@
-//package com.spring.boot.filter;
-//import com.spring.boot.config.jwt.TokenHandler;
-//import com.spring.boot.dto.UsersDto;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//import java.io.IOException;
-//import java.util.List;
-//import java.util.Objects;
-//import java.util.stream.Collectors;
-//@Component
-//public class AuthFilter extends OncePerRequestFilter {
-//
-//    @Autowired
-//    private TokenHandler tokenHandler ;
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        String token = request.getHeader("Authorization");
-//        if (Objects.isNull(token) || !token.startsWith("Bearer")){
-//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//            return;
-//        }
-//        token = token.substring(7);
-//        UsersDto usersDto = tokenHandler.VaildateToken(token);
-//        if (Objects.isNull(usersDto)){
-//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//            return;
-//        }
-//        List<SimpleGrantedAuthority> roles = usersDto.getRoles().stream().map(roleDto -> new SimpleGrantedAuthority("ROLE_" + roleDto.getCode())).collect(Collectors.toList());
-//        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(usersDto, usersDto.getPassword(), roles);
-//        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-//        filterChain.doFilter(request, response);
-//
-//
-//
-//    }
-//
-//
-//    @Override
-//    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-//        String uri = request.getRequestURI();
-//        return uri.contains("/users/login") || uri.contains("/users/signup");
-//    }
-//
-//}
+package com.spring.boot.filter;
+import com.spring.boot.config.jwt.TokenHandler;
+import com.spring.boot.dto.UsersDto;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+@Component
+public class AuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenHandler tokenHandler ;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+        if (Objects.isNull(token) || !token.startsWith("Bearer")){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        token = token.substring(7);
+        UsersDto usersDto = tokenHandler.VaildateToken(token);
+        if (Objects.isNull(usersDto)){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        List<SimpleGrantedAuthority> roles = usersDto.getRoles().stream().map(roleDto -> new SimpleGrantedAuthority("ROLE_" + roleDto.getCode())).collect(Collectors.toList());
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(usersDto, usersDto.getPassword(), roles);
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        filterChain.doFilter(request, response);
+
+
+
+    }
+
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String uri = request.getRequestURI();
+        return uri.contains("/users/login") || uri.contains("/users/signup");
+    }
+
+}
